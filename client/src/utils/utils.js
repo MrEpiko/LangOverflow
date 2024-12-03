c837501f0b218a83a555076cfe105bbdebbfa8d2
Maksim <maksim.basta@gmail.com>
2024-12-03 22:23:37 +0100

    regex at javascript

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,4 +1,5 @@
 export const validateEmail = (email) => {
-  const regex = /^[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]{2,7}$/;
+  const regex = /\b[A-Za-z0-9]+(?:[.\-_][A-Za-z0-9]+)*@[A-Za-z0-9-]+\.[A-Za-z]{2,7}\b/;
   return regex.test(email);
 }
+
