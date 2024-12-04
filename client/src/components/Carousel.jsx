4c1db0e85c7516fcdf246050ecce88aff12a8ead
Matija Djuricic <98923192+MatijaDjuricic@users.noreply.github.com>
2024-12-04 20:05:29 +0100

    fix: carousel styling

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,36 +1,27 @@
-import serbia_img from '../assets/flags/serbia.png';
-import bosnia_img from '../assets/flags/bosnia.png';
-import slovenia_img from '../assets/flags/slovenia.png';
-import america_img from '../assets/flags/america.png';
-import canada_img from '../assets/flags/canada.png';
-import uk_img from '../assets/flags/uk.png';
-import spain_img from '../assets/flags/spain.png';
-import finland_img from '../assets/flags/finland.png';
-import jamaica_img from '../assets/flags/jamaica.png';
-import norway_img from '../assets/flags/norway.png';
-import czech_img from '../assets/flags/czech.png';
-import russia_img from '../assets/flags/russia.png';
-import polland_img from '../assets/flags/polland.png';
+import { flag_images } from "../utils/constants";
 import styles from "../components/Carousel.module.css";
 const Carousel = () => {
-  const images = [
-    serbia_img, bosnia_img, slovenia_img, america_img, canada_img, uk_img, spain_img,
-    finland_img, jamaica_img, norway_img, czech_img, russia_img, polland_img
-  ];
+  const width = '120px';
+  const height = '70px';
+  const quantity = flag_images.length;
+  const sliderStyle = {
+    '--width': width,
+    '--height': height,
+    '--quantity': quantity,
+  }
   return (
     <div className={styles.carouselContainer}>
-      <div className={styles.carouselTrack}>
-        {images.map((image, index) => (
-          <div key={index} className={styles.carouselImage}>
-            <img src={image} alt={`Flag ${index}`} />
-          </div>
-        ))}
-        {images.map((image, index) => (
-          <div key={`duplicate-${index}`} className={styles.carouselImage}>
-            <img src={image} alt={`Flag Duplicate ${index}`} />
-          </div>
-        ))}
-      </div>
+        <div className={styles.slider} style={sliderStyle}>
+            <div className={styles.list}>
+                {
+                    flag_images.map((image, index) => (
+                        <div key={index} style={{'--position': index}} className={styles.item}>
+                            <img src={image} alt={`Flag ${index}`} />
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     </div>
   );
 };
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,20 @@
+import serbia_img from '../assets/flags/serbia.png';
+import bosnia_img from '../assets/flags/bosnia.png';
+import slovenia_img from '../assets/flags/slovenia.png';
+import america_img from '../assets/flags/america.png';
+import canada_img from '../assets/flags/canada.png';
+import uk_img from '../assets/flags/uk.png';
+import spain_img from '../assets/flags/spain.png';
+import finland_img from '../assets/flags/finland.png';
+import jamaica_img from '../assets/flags/jamaica.png';
+import norway_img from '../assets/flags/norway.png';
+import czech_img from '../assets/flags/czech.png';
+import russia_img from '../assets/flags/russia.png';
+import polland_img from '../assets/flags/polland.png';
+export const flag_images = [
+    serbia_img, bosnia_img, slovenia_img, america_img, canada_img, uk_img, spain_img,
+    finland_img, jamaica_img, norway_img, czech_img, russia_img, polland_img
+];
 export const translations = [
   "This", "Esto", "Ceci", "To",
   "Dies", "Dette","Dette", "Eto",
