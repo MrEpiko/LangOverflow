2bddf9135c8811afbd4560820c74556ff9417a1b
Matija Djuricic <98923192+MatijaDjuricic@users.noreply.github.com>
2024-12-05 22:04:30 +0100

    fix: profile page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -28,28 +28,30 @@ const ProfilePage = () => {
                         <img className={styles.profile_picture} src={user.profile_picture ? user.profile_picture : profile_img} alt="Profile" />
                         <div className={styles.userMeta}>
                             <h1>{user.username}</h1>
-                            <h3>id: {user.id}</h3>
+                            <h3>{user.email}</h3>
                         </div>
-                        
                     </div>
                     <div className={styles.redOnes}>
                         <button onClick={logout}>Logout</button>
                         <button onClick={()=>deleteProfile(userId)}>Delete profile</button>
                     </div>
                 </div>
-                <h5>Your questions:</h5>
-                {data && (data.threads.length === 0 ? <h2>You don't have any posted questions.</h2> : <div className={styles.questionLines}>
-                    {data && data.threads.map((thread) => (
-                        <div key={thread.id}>
-                            <QuestionLine thread={thread}/>
-                        </div>
-                    ))}
-                </div> )}
-            </div>
-            
+                {
+                    data && (data.threads.length === 0 ? <h2 className={styles.dontHave}>You don't have any posted questions.</h2> :
+                        <div className={styles.questionLines}>
+                            <h5>Your questions:</h5>
+                        {
+                            data && data.threads.map((thread) => (
+                                <div key={thread.id}>
+                                    <QuestionLine thread={thread}/>
+                                </div>
+                            ))
+                        }
+                    </div> 
+                )}
+            </div> 
         </div>
         </>
-        
     );
 };
 export default ProfilePage;
